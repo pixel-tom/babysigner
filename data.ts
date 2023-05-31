1eacae9cff5aa383836f75d5c0d850e49576cbcf
Bnor2710 <bnor2710@gmail.com>
2023-05-30 23:43:02 -0500

    add assets

--- data.ts
+++ data.ts
@@ -2,7 +2,9 @@
 import { CharacterParts } from "./types";
 
 export const characterParts: CharacterParts = {
-  background: [{ id: 0, name: "Blue", image: "/assets/background/blue.png" }],
+  background: [
+    { id: 0, name: "Blue", image: "/assets/background/blue.png" }
+  ],
   type: [
     { id: 0, name: "Alien", image: "/assets/type/alien.png" },
     { id: 1, name: "Dark", image: "/assets/type/dark.png" },
@@ -14,17 +16,45 @@ export const characterParts: CharacterParts = {
     { id: 7, name: "Solana", image: "/assets/type/solana.png" },
     { id: 8, name: "Zombie", image: "/assets/type/zombie.png" },
   ],
-  hats: [{ id: 0, name: "None", image: "/assets/hats/none.png" }],
-  clothes: [{ id: 0, name: "None", image: "/assets/clothes/none.png" }],
+  hats: [
+    { id: 0, name: "None", image: "/assets/hats/none.png" }
+  ],
+  clothes: [
+    { id: 0, name: "None", image: "/assets/clothes/none.png" },
+    { id: 1, name: "Poncho", image: "/assets/clothes/poncho.png" },
+    { id: 2, name: "Red Shirt", image: "/assets/clothes/red-shirt.png" },
+    { id: 3, name: "Purple Shirt", image: "/assets/clothes/purple-shirt.png" },
+    { id: 4, name: "Blue Shirt", image: "/assets/clothes/blue-shirt.png" },
+    { id: 5, name: "Green Shirt", image: "/assets/clothes/green-shirt.png" },
+    { id: 6, name: "Brown Shirt", image: "/assets/clothes/brown-shirt.png" },
+    { id: 7, name: "Orange Shirt", image: "/assets/clothes/orange-shirt.png" },
+    { id: 8, name: "Hawaiian Shirt", image: "/assets/clothes/hawaiian-shirt.png" },
+    { id: 9, name: "Sailor Shirt", image: "/assets/clothes/sailor-shirt.png" },
+    { id: 10, name: "Suspenders", image: "/assets/clothes/suspenders.png" },
+    { id: 11, name: "Armor", image: "/assets/clothes/armor.png" },
+    { id: 12, name: "Baby Vest", image: "/assets/clothes/baby-vest.png" },
+    { id: 13, name: "Military Vest", image: "/assets/clothes/military-vest.png" },
+    { id: 14, name: "Stunt Suit", image: "/assets/clothes/stunt-suit.png" },
+],
   eyes: [
     { id: 0, name: "None", image: "/assets/eyes/none.png" },
     { id: 1, name: "Cool Glasses", image: "/assets/eyes/cool-glasses.png" },
     { id: 2, name: "Nerd Glasses", image: "/assets/eyes/nerd-glasses.png" },
     { id: 3, name: "Purple Glasses", image: "/assets/eyes/purple-glasses.png" },
     { id: 4, name: "Yellow Glasses", image: "/assets/eyes/yellow-glasses.png" },
-    { id: 5, name: "3d-glasses", image: "/assets/eyes/3d-glasses.png" },
+    { id: 5, name: "3D Glasses", image: "/assets/eyes/3d-glasses.png" },
+    { id: 6, name: "Sunset Vipers", image: "/assets/eyes/sunset-vipers.png" },
+    { id: 7, name: "Solana Vipers", image: "/assets/eyes/solana-vipers.png" },
+    { id: 8, name: "VR Goggles", image: "/assets/eyes/vr-goggles.png" },
+    
   ],
-  mouth: [{ id: 0, name: "None", image: "/assets/mouth/none.png" }],
-  special: [{ id: 0, name: "None", image: "/assets/special/none.png" }],
-  vr: [{ id: 0, name: "None", image: "/assets/vr/none.png" }],
+  mouth: [
+    { id: 0, name: "None", image: "/assets/mouth/none.png" }
+  ],
+  special: [
+    { id: 0, name: "None", image: "/assets/special/none.png" }
+  ],
+  vr: [
+    { id: 0, name: "None", image: "/assets/vr/none.png" }
+  ],
 };
